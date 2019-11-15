75f1cb765619635cf59c452b8ddb5a17aea148e6
alhajiry <latiforce@gmail.com>
2019-11-15 16:00:00 +0700

    Updates

---
+++ precache-manifest.8f0a95db34a84d174ceace529848d7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3f31e32b0ae6c2e078a3b6f49dac1e",
+    "url": "/project-react-counter/index.html"
+  },
+  {
+    "revision": "2ff9a1506d8fce2fe918",
+    "url": "/project-react-counter/static/css/main.de5ca3e7.chunk.css"
+  },
+  {
+    "revision": "85914441d33eddce14ba",
+    "url": "/project-react-counter/static/js/2.5d2507d2.chunk.js"
+  },
+  {
+    "revision": "2ff9a1506d8fce2fe918",
+    "url": "/project-react-counter/static/js/main.bd5b33cf.chunk.js"
+  },
+  {
+    "revision": "416efcd61637251b573c",
+    "url": "/project-react-counter/static/js/runtime-main.fbd606e0.js"
+  },
+  {
+    "revision": "4c2b95987792e593d054d4b0b2de7b99",
+    "url": "/project-react-counter/static/media/logo-top.4c2b9598.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/project-react-counter/static/media/logo.25bf045c.svg"
+  }
+]);
